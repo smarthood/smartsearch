640a7bc83c1723a5b0f748f3a8adeaa91ebcdf5e
smarthood <renolddickson18@gmail.com>
2022-06-16 11:55:26 +0530

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input,setInput] = useState("")
+  const [key,setKey]=useState("flutter")
+  const handleKey=(k)=>{
+    setKey(k.target.value)
+  }
+  const handleInput =(e)=>{
+    setInput(e.target.value)
+  }
+  const search =()=>{
+    if(input==""){
+      alert("Invalid input")
+    }
+    else{
+      window.open('http://google.com/search?q=how to '+input+' in '+key)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="search_bar">
+       <input type="text" placeholder='search here...' onChange={handleInput}/>
+       <select onChange={handleKey}>
+         <option value="flutter">Flutter</option>
+         <option value="react js"> React js</option>
+         <option value="react native">React Native</option>
+         <option value="firebase">Firebase</option>
+         <option value="html">HTML</option>
+         <option value="css">CSS</option>
+         <option value="javascript">JS</option>
+       </select><br />
+       <span><button onClick={search}>search</button></span>
+     </div>
+     <div className="logo">
+       <img src={"./images/"+key+".png"} alt={key} />
+     </div>
+     <div className="design">
+       <div className="tri1"></div>
+       <div className="tri2"></div>
+     </div>
     </div>
   );
 }
